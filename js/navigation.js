52f39b94c7515bb35e628a12a54e3a020e6fdd69
Katrine <clarissekatrine.molon01@gmail.com>
2021-11-24 18:13:10 -0600

    first commit

---
+++ js/navigation.js
@@ -0,0 +1,12 @@
+
+document.getElementById("navigation").innerHTML =  `<div id="navigation">
+<ul>
+    <li><a href="index.html">Home</a></li>
+    <li><a href="about.html">about me</a></li>
+    <li><a href="contact.asp">Family background</a></li>
+    <li><a href="about.asp">Educational background</a></li>
+    <li><a href="news.asp">Educational Attainment</a></li>
+    <li><a href="contact.asp">Favourites</a></li>
+    <li><a href="about.asp">Photo Gallery</a></li>
+</ul>
+</div>`
